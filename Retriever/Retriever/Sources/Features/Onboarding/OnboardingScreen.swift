acbd15570a24f7bb2681ec24341a79d594aa057c
choijunyeong-company <junyeong.choi@nhn.com>
2026-01-21 10:25:22 +0900

    Feat, 코드 생성

--- Retriever/Retriever/Sources/Features/Onboarding/OnboardingScreen.swift
+++ Retriever/Retriever/Sources/Features/Onboarding/OnboardingScreen.swift
@@ -17,11 +17,14 @@ extension OnboardingScreen {
         super.viewDidLoad()
         setupUI()
         reactor.send(.viewDidLoad)
+        guard let _ = 1 as? Int else { return }
     }
 }
 
 extension OnboardingScreen {
     private func setupUI() {
         view.backgroundColor = .systemBackground
+        
+        guard let _ = 1 as? Int else { return }
     }
 }
--- Retriever/Retriever/Sources/Features/Root/RootProvider.swift
+++ Retriever/Retriever/Sources/Features/Root/RootProvider.swift
@@ -8,5 +8,6 @@ import CoreFlow
 struct RootProvider {
     func provide(_ serviceLocator: ServiceLocator) {
         // Root에서 사용할 의존성 등록
+        guard let _ = 1 as? Int else { return }
     }
 }
